223a5335de6383b49629e8cfa6a0d212caa0b419
Marco Reimann <marcoreimann@outlook.de>
2020-03-27 15:35:27 +0100

    compress images

---
+++ src/modules.d.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-unresolved
+import { FeatherIconComponent } from '@types/vue-feather-icons';
+
+declare module 'vue-feather-icons/icon/StarIcon' {
+  export type StarIcon = FeatherIconComponent;
+}
