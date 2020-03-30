d1e680cb50c82e004f0b844b393ecd8efaccd7c8
Marco Reimann <marcoreimann@outlook.de>
2020-03-30 21:04:08 +0200

    buttons for after game navigation

--- src/views/GameWon.vue
+++ src/views/GameWon.vue
@@ -5,6 +5,21 @@
     <div v-if="gameOver !== false" class="container">
       <h3 class="mb-2 font-old text-4xl">{{ gameOver.party }}s have won the game.</h3>
       <p>{{ gameOver.reason }}</p>
+
+      <div class="mt-8">
+        <div class="flex justify-center -mx-4">
+          <div class="px-4">
+            <router-link class="btn shadow" :to="{ name: Route.GameCreate }">
+              Create a new game
+            </router-link>
+          </div>
+          <div class="px-4">
+            <router-link class="btn shadow" :to="{ name: Route.GameJoin }">
+              Join a new game
+            </router-link>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +28,7 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { mapGetters } from 'vuex';
+import Route from '@/types/route';
 import { Party } from '../types/game';
 
 @Component({
@@ -22,6 +38,8 @@ import { Party } from '../types/game';
 })
 export default class GameWon extends Vue {
   gameOver!: { party: Party, reason: string };
+
+  Route = Route;
 
   mounted() {
     this.$gtag.event('game', {
